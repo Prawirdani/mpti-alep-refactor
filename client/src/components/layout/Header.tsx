e487f80bd494eafa2681b2bf6aed0b85745b0ece
Prawirdani <prawirdani@gmail.com>
2024-06-04 05:48:20 +0700

    fix: text color typo

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -6,16 +6,16 @@ export default function Header() {
       <div className="flex justify-between gap-2 w-full">
         <h1 className="text-primary font-bold text-xl">Sultan Barbershop</h1>
         <div className="flex gap-4 items-center">
-          <Link to="/" className="text-primary font-semibold hover:text-seconday">
+          <Link to="/" className="text-primary font-semibold hover:text-secondary">
             Home
           </Link>
-          <Link to="/about" className="text-primary font-semibold hover:text-seconday">
+          <Link to="/about" className="text-primary font-semibold hover:text-secondary">
             About
           </Link>
-          <Link to="/contact" className="text-primary font-semibold hover:text-seconday">
+          <Link to="/contact" className="text-primary font-semibold hover:text-secondary">
             Contact
           </Link>
-          <Link to="/booking" className="text-primary font-semibold hover:text-seconday">
+          <Link to="/booking" className="text-primary font-semibold hover:text-secondary">
             Booking
           </Link>
         </div>
