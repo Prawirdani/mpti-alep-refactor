e65819c266157d337f3cf546f1133f8b58b42fd0
Prawirdani <prawirdani@gmail.com>
2024-06-04 03:46:01 +0700

    add paket, karyawan & transaksi type

--- client/src/app.d.ts
+++ client/src/app.d.ts
@@ -1,13 +1,46 @@
 // app.d.ts
 declare global {
-	type UserRole = 'admin' | 'operator';
+  type UserRole = 'admin' | 'operator';
 
-	type AuthUser = {
-		id: number;
-		nama: string;
-		username: string;
-		role: UserRole;
-	};
+  type AuthUser = {
+    id: number;
+    nama: string;
+    username: string;
+    role: UserRole;
+  };
+
+  type Karyawan = {
+    id: number;
+    nama: string;
+    handphone: string;
+    created_at: string;
+    updated_at: string;
+    deleted: boolean;
+  };
+
+  type Paket = {
+    id: number;
+    nama: string;
+    deskripsi: string;
+    harga: number;
+    created_at: string;
+    updated_at: string;
+    deleted: boolean;
+  };
+
+  type Transaksi = {
+    id: number;
+    nama_customer: string;
+    no_hp: string;
+    paket_id: number;
+    karyawan_id: number;
+    status: string;
+    total_harga: number;
+    jadwal_booking: string;
+    waktu_transaksi: string;
+    karyawan: Karyawan;
+    paket: Paket;
+  };
 }
 
-export { };
+export {};
