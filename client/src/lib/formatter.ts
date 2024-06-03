15d0159297a549ba4a9df7813cc4df024887767e
Prawirdani <prawirdani@gmail.com>
2024-06-04 03:44:52 +0700

    update: time formatter

--- client/src/lib/formatter.ts
+++ client/src/lib/formatter.ts
@@ -1,3 +1,16 @@
 export function formatIDR(number: number): string {
   return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(number);
 }
+
+export function formatTime(date: Date): string {
+  const formatter = new Intl.DateTimeFormat('id-ID', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  });
+  return formatter.format(date);
+}
