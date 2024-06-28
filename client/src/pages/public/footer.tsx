716c383e5bee8b3db2590f64d57d6d4132e4d711
Prawirdani <prawirdani@gmail.com>
2024-06-29 00:41:20 +0700

    rm footer border

--- client/src/pages/public/footer.tsx
+++ client/src/pages/public/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="z-40 p-4 border-t flex justify-center items-center">
+    <footer className="z-40 p-4 flex justify-center items-center">
       <div className="flex items-center gap-2">
         <p>© 2024 SultanBarberShop</p>
       </div>
