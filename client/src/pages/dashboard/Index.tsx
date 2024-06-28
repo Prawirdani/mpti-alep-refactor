2c5a91e8544a0b4151f06bde87b2fe4561795ccc
Prawirdani <prawirdani@gmail.com>
2024-06-29 05:35:12 +0700

    add additional boilerplate

--- client/src/pages/dashboard/Index.tsx
+++ client/src/pages/dashboard/Index.tsx
@@ -5,11 +5,15 @@ export default function Index() {
   return (
     <section>
       <TitleSetter title="Dashboard" />
-      <div className="grid sm:grid-cols-2 xl:grid-cols-4 gap-8">
-        <Card className="p-4 rounded-sm min-h-44">This is a card</Card>
-        <Card className="p-4 rounded-sm min-h-44">This is a card</Card>
-        <Card className="p-4 rounded-sm min-h-44">This is a card</Card>
-        <Card className="p-4 rounded-sm min-h-44">This is a card</Card>
+      <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-8">
+        <Card className="p-4 rounded-sm min-h-44 flex place-items-center justify-center">Key Metric 1</Card>
+        <Card className="p-4 rounded-sm min-h-44 flex place-items-center justify-center">Key Metric 2</Card>
+        <Card className="p-4 rounded-sm min-h-44 flex place-items-center justify-center">Key Metric 3</Card>
+        <Card className="p-4 rounded-sm min-h-44 flex place-items-center justify-center">Key Metric 4</Card>
+        <Card className="p-4 rounded-sm h-[400px] md:col-span-2 xl:col-span-3 flex place-items-center justify-center">
+          Graph goes here
+        </Card>
+        <Card className="p-4 rounded-sm h-[400px] flex place-items-center justify-center">Additional</Card>
       </div>
     </section>
   );
